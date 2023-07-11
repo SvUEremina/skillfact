b042c24f2be51b4c845ec79de04dd35e8e8c5360
SvUEremina <129377617+SvUEremina@users.noreply.github.com>
2023-07-11 15:15:03 +0500

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -6,13 +6,13 @@ class TestCalc:
         self.calc = Calculator
 
     def test_multiple_calculate_correctly(self):
-        assert self.calc.multiply(self, 2,2) == 4
+        assert self.calc.multiply(self, 2,3) == 6
 
     def test_division_calculate_correctly(self):
-        assert self.calc.division(self, 4,2) == 2
+        assert self.calc.division(self, 8,2) == 4
 
     def test_subtraction_calculate_correctly(self):
-        assert self.calc.subtraction(self, 4,2) == 2
+        assert self.calc.subtraction(self, 9,3) == 6
 
     def test_adding_calculate_correctly(self):
-        assert self.calc.adding(self, 4,2) == 6
+        assert self.calc.adding(self, 4,3) == 7
